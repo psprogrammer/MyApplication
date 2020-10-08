8fdff9a3d18f5029bce1f91f51e11acda07ff9c5
Pooja Singh <poojasingh@juniper.net>
2020-10-08 17:21:45 +0530

    folder removed

--- FileUpload-master/src/upload/UploadComponent.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-interface FormProps {
-  filename: string;
-}
-
-export const UploadForm: React.SFC<FormProps> = props => (
-  <h1>Hello {props.filename}</h1>
-);
--- FileUpload-master/src/upload/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Route } from 'react-router';
-
-import { UploadPage } from './UploadContainer';
-
-export const uploadRoutes = () => (
-  <Route path="/upload" component={UploadPage} />
-);
